a9474742d301fe300bc1f2caedde7a4ed4a7131f
nrnarayan <nrnarayan@ucdavis.edu>
2014-06-18 14:37:29 -0700

    first commit

---
+++ test1.R
@@ -0,0 +1,6 @@
+# this is test file #1
+# will allow version control on analysis for specific projects
+
+x <- 2+4
+y <- 0+4
+x+y
---
+++ test2.R
@@ -0,0 +1,6 @@
+# this is test file #2
+# will allow version control on analysis for specific projects
+
+a <- 2+4
+b <- 0+4
+a+b
